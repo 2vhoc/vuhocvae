4271f1f7db8e0636a2a3e005378529ebac8e0892
Hoc Vu Van <2vhoc7@gmail.com>
2024-07-29 19:47:31 +0700

    Auto Python

---
+++ main.py
@@ -0,0 +1,62 @@
+#Library SELENIUM:
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+
+#Libreary SYSTEM:
+import os
+import time
+
+#Source:
+WChromeOP = webdriver.ChromeOptions()
+pref = {
+    "profile.managed_default_content_setting.images": 2
+}
+#Main
+if __name__ == "__main__":
+    try:
+        WChromeOP.add_experimental_option("prefs", pref)
+        driver_path = r'D:\Code\Python\AutoPython\chromedriver.exe'
+        service1 = Service(driver_path)
+        driver = webdriver.Chrome(service=service1, options=WChromeOP)
+        driver.get(url='https://www.facebook.com')
+#ID_PASS:
+        time.sleep(1)
+        css_id = driver.find_element(By.CSS_SELECTOR, 'input[name="email"]')
+        css_id.send_keys('0876756956')
+        time.sleep(0.5)
+        css_pass = driver.find_element(By.CSS_SELECTOR, 'input[name="pass"]')
+        css_pass.send_keys('Vuhoc..2005')
+        css_button_login = driver.find_element(By.CSS_SELECTOR, 'button[name="login"]')
+        css_button_login.click()
+        WebDriverWait(driver, 10).until(ec.presence_of_element_located((By.CSS_SELECTOR, '[aria-label="Messenger"]')))
+#Body:
+        css_messenger_button = driver.find_element(By.CSS_SELECTOR, '[aria-label="Messenger"]')
+        css_messenger_button.click()
+        element = WebDriverWait(driver, 10).until(
+            ec.presence_of_element_located((By.XPATH, '//span[contains(text(), "Vu Hoc")]'))
+        )
+        element.click()
+
+        div_element = WebDriverWait(driver, 10).until(
+        ec.presence_of_element_located((By.CSS_SELECTOR, 'div[contenteditable="true"]'))
+    )
+#Auto:
+        time.sleep(1)
+        for _ in range(1000):
+            div_element.click()
+            div_element.send_keys("Ôi ôi ôi, mất quyền kiểm soát, mất quyền kiểm soát, ôi ôi ôi")
+            time.sleep(0.5)
+            div_element.send_keys(Keys.ENTER)
+    except:
+        pass
+    
+
+
+
+#Keep webchrome
+    while True:
+        time.sleep(10)
